23fb3a438d97d7cb36bab54a5f9212ddbdda43c7
Yash Jain <nannujain2005@gmail.com>
2025-07-14 15:14:51 +0530

    14th July Progress

---
+++ MachineLearning/NLP/ANN_Classification_Project/app_r.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+
+## Loading the model
+model = load_model('regression.keras')
+
+
+## OPening all the encoders
+
+with open('OneHotEncoder.pkl', 'rb') as file:
+    OHE = pickle.load(file)
+with open('scaler.pkl', 'rb') as file:
+    scaler = pickle.load(file)
+with open('label_encoder.pkl', 'rb') as file:
+    label_encoder = pickle.load(file)
+
+## Streamlit App
+
+st.title('Customer Salary Prediction')
+
+geography = st.selectbox('Geography', OHE.categories_[0])
+gender = st.selectbox('Gender', label_encoder.classes_)
+age = st.slider('Age', 18,92)
+balance = st.number_input('Balance')
+credit_score = st.number_input('Credit Score')
+tenure = st.slider('Tenure', 1,10)
+num_of_products = st.slider('Number of Products', 1,4)
+has_cr_card = st.selectbox('Has Credit Card', [0,1])
+is_active_member = st.selectbox('Is Active Member',[0,1])
+exited = st.selectbox('Exited', [0,1])
+
+
+input_data = pd.DataFrame({
+    'CreditScore': [credit_score],
+    'Gender': [label_encoder.transform([gender])[0]],
+    'Age':[age],
+    'Tenure' : [tenure],
+    'Balance' : [balance],
+    'NumOfProducts' :[num_of_products],
+    'HasCrCard' : [has_cr_card],
+    'IsActiveMember':[is_active_member],
+    'Exited' : [exited]
+
+})
+
+## one hot encoding
+geo_encoded = OHE.transform([[geography]]).toarray()
+geo_encoded_df = pd.DataFrame(geo_encoded, columns = OHE.get_feature_names_out(['Geography']))
+
+input_data = pd.concat([input_data.reset_index(drop=True),geo_encoded_df],axis=1)
+
+##scaling
+
+scaled_data = scaler.transform(input_data)
+
+prediction = model.predict(scaled_data)
+prob = prediction[0][0]
+
+st.write(f'Salary Estimated, {prob:.2f}')
+
